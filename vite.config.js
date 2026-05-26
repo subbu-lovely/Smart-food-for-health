3be63eeca66a31fead3268ae32fab996092c9fe7
unknown <subbukondrupathi@gmail.com>
2026-05-26 12:03:00 +0530

    fixed github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/smart-food-for-health/',
+  base: '/Smart-food-for-health/',
 })
